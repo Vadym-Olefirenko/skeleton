1a2e5cae5ae1429160add143ec02d5c93b0b56d7
Vadym-Olefirenko <53852649+Vadym-Olefirenko@users.noreply.github.com>
2020-08-02 20:12:34 +0300

    Update webpack.config.js
    Добавил бабел и работу с асинхронным кодом

--- webpack.config.js
+++ webpack.config.js
@@ -16,35 +16,31 @@ module.exports = {
 
   module: {
     rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+          plugins: [
+             '@babel/plugin-proposal-class-properties',
+             '@babel/transform-runtime',
+            ]
+        }
+      }
+    },
+       {
        test:/\.(s*)css$/,
-       use: [ 
+       use: [
           miniCss.loader,
           'css-loader',
-          {
-            loader: 'resolve-url-loader',
-            options: {
-              root: path.join(__dirname, 'src')
-            }
-          },
           'sass-loader',
        ]
     },
-    { 
-      test: /\.(png|jpg|gif)$/,
-      use: [
-          {
-              loader: 'file-loader',
-              options: {
-                  name: '[path][name].[ext]',
-                  context: path.resolve(__dirname, "src/"),
-                  outputPath: '',
-                  publicPath: './',
-                  useRelativePaths: true
-              }
-          }
-      ] 
-  }
-   ]
+    {
+      test: /\.txt$/i,
+      use: 'raw-loader',
+    }]
  },
  plugins: [
     new miniCss({
